ca9ae2346b7b51a8f3adcf6378300197c6275b2f
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-10-04 14:49:51 +0900

    Update __init__.py

--- wradex/__init__.py
+++ wradex/__init__.py
@@ -1,1 +1,16 @@
 # coding: utf-8
+
+# dependent packages
+import shutil
+
+# submodules
+from .constants import *
+from .radex import *
+
+# create ~/.wradex
+if not WRADEX_DIR.exists():
+    WRADEX_DIR.mkdir()
+    shutil.copy(_WRADEX_CONF, WRADEX_DIR)
+
+# clean local
+del shutil
