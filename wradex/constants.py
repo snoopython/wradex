6f4f07b03d04bbb484b63a6cd4b42f16ec6dbc11
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-10-04 14:49:35 +0900

    Update constants.py

--- wradex/constants.py
+++ wradex/constants.py
@@ -1,1 +1,18 @@
 # coding: utf-8
+
+__all__ = [
+    'WRADEX_DIR',
+    'WRADEX_CONF',
+    '_WRADEX_CONF',
+]
+
+# standard library
+from pathlib import Path
+
+# dependent packages
+import wradex
+
+# local constants
+WRADEX_DIR   = Path('~', 'wradex').expanduser()
+WRADEX_CONF  = Path(WRADEX_DIR, 'config.yaml')
+_WRADEX_CONF = Path(*wradex.__path__, 'data', 'config.yaml')
