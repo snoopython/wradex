759acdcd4d87175e7e1159cdcffbf241c1fa390f
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2019-04-23 17:48:40 +0900

    :tada: Release v0.2

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(name = 'wradex',
       description = 'RADEX wrapper written in Python 3',
-      version = '0.1',
+      version = '0.2',
       author = 'astropenguin',
       author_email = 'taniguchi@a.phys.nagoya-u.ac.jp',
       url = 'https://github.com/astropenguin/wradex',
       keywords = 'astronomy radex wrapper',
       packages = ['wradex'],
       package_data = {'wradex': ['data/*.yaml']},
-      install_requires = ['astropy', 'numpy', 'pyyaml'])
+      install_requires = ['astropy', 'numpy', 'pyyaml', 'tqdm'])
